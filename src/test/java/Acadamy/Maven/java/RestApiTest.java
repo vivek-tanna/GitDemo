de682e1b27c8656b1a3b508882ebb39f919d50c8
vivek-tanna <103312820+vivek-tanna@users.noreply.github.com>
2022-04-09 19:14:36 +0530

    Update RestApiTest.java

--- src/test/java/Acadamy/Maven/java/RestApiTest.java
+++ src/test/java/Acadamy/Maven/java/RestApiTest.java
@@ -9,5 +9,11 @@ public class RestApiTest {
 	public void MavenruningTest()
 	{
 		System.out.println("Running RestAPITest..........");
+		System.out.println("Running RestAPITest1..........");
+		System.out.println("Running RestAPITest2..........");
+		System.out.println("Running RestAPITest3..........");
+		System.out.println("Running RestAPITest4..........");
+		System.out.println("Running RestAPITest5..........");
+		System.out.println("Running RestAPITest6..........");
 	}
 }
